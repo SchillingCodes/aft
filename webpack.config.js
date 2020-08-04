d5297caa610827cadc21152e44a13e5de773e613
SchillingCodes <tamakuchiko@gmail.com>
2020-08-04 11:50:01 -0400

    Some more setup/learning

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const webpack = require("webpack");
+const {merge} = require("webpack-merge");
+const loadPresets = require("./build-utils/presets/loadPresets")
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-};
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) =>
+  merge(
+    {
+      entry: "./src/index.js",
+      mode: mode,
+      output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+      },
+      plugins: [new HtmlWebpackPlugin()]
+    },
+    modeConfig(mode)
+  );
